b97dbf91c0fcdc017d4530209106db2964be99b2
BAEK0111 <143854581+BAEK0111@users.noreply.github.com>
2025-07-01 14:28:54 +0900

    [Bronze V] Title: 아스키 코드, Time: 180 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char a = input.nextLine().charAt(0);
+        int num = (int)a;
+        System.out.println(num);
+    }
+}
