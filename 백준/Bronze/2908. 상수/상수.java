0291278d44b5591ce97b0706a2bd807a1b7885ac
BAEK0111 <143854581+BAEK0111@users.noreply.github.com>
2025-07-01 19:11:29 +0900

    [Bronze II] Title: 상수, Time: 164 ms, Memory: 17520 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -3,17 +3,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int m = input.nextInt();
-        int n2 = reverse(n);
-        int m2 = reverse(m);
-        int result = Math.max(n2, m2);
-        System.out.println(result);
-    }
-    public static int reverse(int k) {
-        int a = k % 10;
-        int c = k / 100;
-        int b = k / 10 - c * 10;
-        return a * 100 + 10*b + c;
+        String n = new StringBuilder(input.next()).reverse().toString();
+        String m = new StringBuilder(input.next()).reverse().toString();
+
+        int n2 = Integer.parseInt(n);
+        int m2 = Integer.parseInt(m);
+
+        System.out.println(Math.max(n2, m2));
     }
 }
