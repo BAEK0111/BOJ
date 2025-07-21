427b1cb39044a6b4d5686404a90c0c15af160dcc
BAEK0111 <143854581+BAEK0111@users.noreply.github.com>
2025-07-21 14:57:33 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 168 ms, Memory: 17608 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,18 +1,10 @@
 import java.util.*;
-import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String vocab = input.next();
-        int k = 1;
-
-        for(int i = 0; i < vocab.length() / 2; i++){
-            if(vocab.charAt(i) != vocab.charAt(vocab.length() - 1 - i)){
-                k = 0;
-                break;
-            }
-        }
-        System.out.println(k);
+        String reversed = new StringBuilder(vocab).reverse().toString();
+        System.out.println(vocab.equals(reversed) ? 1 : 0);
     }
 }
