2cc10425d82f8ac2304222647370267a11de7425
BAEK0111 <143854581+BAEK0111@users.noreply.github.com>
2025-12-31 14:56:59 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 144 ms, Memory: 15852 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,40 +1,35 @@
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int[][] arr1 = new int[n][m];
-        int[][] arr2 = new int[n][m];
+        int[][] arr = new int[n][m];
 
         for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                arr1[i][j] = sc.nextInt();
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                arr2[i][j] = sc.nextInt();
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[][] result = new int[n][m];
-
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                result[i][j] = arr1[i][j] + arr2[i][j];
+                int sum = arr[i][j] + Integer.parseInt(st.nextToken());
+                sb.append(sum).append(" ");
             }
+            sb.append("\n");
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(result[i][j] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println(sb);
     }
 }
