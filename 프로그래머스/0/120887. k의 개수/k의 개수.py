c3f70adf93b0477eb539a9d9a051c5bbd0d5f933
BAEK0111 <143854581+BAEK0111@users.noreply.github.com>
2026-07-26 11:28:15 +0900

    [level 0] Title: k의 개수, Time: 9.43 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/0/120887. k의 개수/k의 개수.py
@@ -1,8 +1,2 @@
 def solution(i, j, k):
-    cnt = 0
-    for m in range(i, j+1):
-        for n in range(0, len(str(m))):
-            if str(k) == str(m)[n]:
-                cnt+=1
-            
-    return cnt
+    return sum(str(i).count(str(k)) for i in range(i, j+1))
