bdde8a8589c3932bc393f53354edb7d183f9d6c8
BAEK0111 <143854581+BAEK0111@users.noreply.github.com>
2026-07-27 14:41:41 +0900

    [level 0] Title: 소인수분해, Time: 0.60 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,13 @@
+def solution(n):
+    prime = [True] * (n+1)
+    prime[0] = False
+    prime[1] = False
+    
+    for i in range(2, int(n**0.5) + 1):
+        if prime[i]:
+            for j in range(i*i, n+1, i):
+                prime[j] = False
+    
+    res = [i for i in range(2, n+1) if prime[i]]
+    
+    return [i for i in res if n % i == 0]
