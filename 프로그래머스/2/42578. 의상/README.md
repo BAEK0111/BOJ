2445f91cc7f6da61ca87be11f07f8692c0943cc5
BAEK0111 <143854581+BAEK0111@users.noreply.github.com>
2026-08-04 17:52:41 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 0.04 ms
+메모리: 87.8 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 04일 17:34:45
+2026년 08월 04일 17:52:36
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap();
+        for(int i = 0; i < clothes.length; i++) {
+            String type = clothes[i][1];
+            if (!map.containsKey(type)) {
+                map.put(type, 2);
+            } else {
+                map.put(type, map.get(type) + 1);
+            }
+        }
+        
+        int result = 1;
+        for (int value : map.values()) {
+           result *= value;
+        }
+        
+        return result - 1;
+    }
+}
