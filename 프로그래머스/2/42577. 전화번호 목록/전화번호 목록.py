cb65b96abd9b800e7452a54fb9ce04e28b5229cc
BAEK0111 <143854581+BAEK0111@users.noreply.github.com>
2026-08-04 17:17:42 +0900

    [level 2] Title: 전화번호 목록, Time: 84.16 ms, Memory: 28.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,8 @@
 def solution(phone_book):
     sorted_phone_book = sorted(phone_book)
     
-    for i in range(1, len(sorted_phone_book)):
-        if sorted_phone_book[i].startswith(sorted_phone_book[i-1]):
+    for p1, p2 in zip(sorted_phone_book, sorted_phone_book[1:]):
+        if p2.startswith(p1):
             return False
 
     return True
