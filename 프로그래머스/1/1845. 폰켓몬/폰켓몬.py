7ce431dfe6dbdf8e0ed5aa8ff76e71e9338bed0b
BAEK0111 <143854581+BAEK0111@users.noreply.github.com>
2026-08-04 16:28:20 +0900

    [level 1] Title: 폰켓몬, Time: 0.60 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,6 +1,2 @@
 def solution(nums):
-    set_nums = list(set(nums))
-    if len(set_nums) >= len(nums) // 2:
-        return len(nums) // 2
-        
-    return len(set_nums)
+    return min(len(set(nums)), len(nums) // 2)
